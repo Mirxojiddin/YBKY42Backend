c6765292dbf2c01a8ced6577eb6e90f322a32dae
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-06-19 19:58:18 +0500

    RoomAvailabilityTestCase

--- booking_rooms/tests.py
+++ booking_rooms/tests.py
@@ -1,8 +1,8 @@
-
+from datetime import datetime
 
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
-from booking_rooms.models import Room
+from booking_rooms.models import Room, RoomAvailability
 
 
 class RoomListApiTestCase(APITestCase):
@@ -167,3 +167,53 @@ class RoomDetailApiTestCase(APITestCase):
         self.assertEqual(response.data['name'], self.room_three.name)
         self.assertEqual(response.data['type'], self.room_three.type)
         self.assertEqual(response.data['capacity'], self.room_three.capacity)
+
+
+class RoomAvailabilityTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.room_one = Room.objects.create(name='new', type='conference', capacity=15)
+        self.room_two = Room.objects.create(name='old', type='team', capacity=8)
+        date = datetime.date.today() + datetime.timedelta(days=10)
+        self.room_availability_one = RoomAvailability.objects.create(room=self.room_one, start=f'{date} 8:00:00',
+                                                                     end=f'{date} 10:00:00')
+        self.room_availability_two = RoomAvailability.objects.create(room=self.room_one, start=f'{date} 10:00:00',
+                                                                     end=f'{date} 11:00:00')
+
+    def test_room_availability_error(self):
+        # room not found
+        response = self.client.get(reverse("booking_rooms:availability", kwargs={"pk": 10}))
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['message'], 'Bunday xona topilmadi')
+
+        # bad date format
+        response = self.client.get(
+            reverse("booking_rooms:availability", kwargs={"pk": self.room_one.id}) + '?date=06-07-2023')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['error'], "Iltimos 'date' ni yil - oy - kun formatida kiriting")
+
+        # date < today
+        response = self.client.get(
+            reverse("booking_rooms:availability", kwargs={"pk": self.room_one.id}) + '?date=2023-05-05')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['error'], "Iltimos bugundan avvalgi kunni kiritmang")
+
+    def room_availability_list(self):
+        # date time not found
+        today = datetime.date.today() + datetime.timedelta(days=10)
+        response = self.client.get(
+            reverse("booking_rooms:availability", kwargs={"pk": self.room_one.id}))
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['message'], f"Xonaning {today} kuni uchun bosh vaqtlari topilmadi")
+
+        # list
+        date_to_search = datetime.date.today() + datetime.timedelta(days=10)
+        response = self.client.get(
+            reverse("booking_rooms:availability", kwargs={"pk": self.room_one.id}), f"?date={date_to_search}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['date'], self.room_availability_one.date)
+        self.assertEqual(response.data[0]['start'], self.room_availability_one.start)
+        self.assertEqual(response.data[0]['end'], self.room_availability_one.end)
+        self.assertEqual(response.data[1]['date'], self.room_availability_two.date)
+        self.assertEqual(response.data[1]['start'], self.room_availability_two.start)
+        self.assertEqual(response.data[1]['end'], self.room_availability_two.end)
