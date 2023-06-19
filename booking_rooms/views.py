76dd966f193da6fe6c6897cdb439751a9ce4da6b
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-06-19 19:47:53 +0500

    BookingRoomApiView

--- booking_rooms/views.py
+++ booking_rooms/views.py
@@ -5,8 +5,8 @@ from django.db.models import Q
 from rest_framework import pagination, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from booking_rooms.serializers import RoomSerializer, RoomAvailabilitySerializer
-from booking_rooms.models import Room, RoomAvailability
+from booking_rooms.serializers import RoomSerializer, RoomAvailabilitySerializer, BookingRoomSerializer
+from booking_rooms.models import Room, RoomAvailability, BookingRoom
 
 
 def check_day(pk):
@@ -98,3 +98,56 @@ class RoomAvailabilityApiView(APIView):
                 return Response(data, status=status.HTTP_404_NOT_FOUND)
         else:
             return result
+
+
+class BookingRoomApiView(APIView):
+    def post(self, request, pk):
+        result = check_day(pk)
+        if not isinstance(result, Room):
+            return result
+        start = request.data.get('start')
+        end = request.data.get('end')
+        resident = request.data.get('resident')
+        if not resident or not (type(resident) is dict):
+            return Response(data={
+                'error': "Iltimos resident ma'lumotlarini kiriting"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        elif 'name' not in resident:
+            return Response(data={
+                'error': "Iltimos resinedtni ismini 'name' orqali kiriting"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        if not start or not end:
+            return Response(data={
+                'error': "Iltimos start va end maydonlarni ikkisini ham kitiring"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        date_format = '%Y-%m-%d %H:%M:%S'
+        try:
+            start_date = datetime.strptime(start, date_format)
+            end_time = datetime.strptime(end, date_format)
+        except ValueError:
+            data = {
+                "error": "Iltimos sanalarnini y-o-k s-d-sek formatida kiriting"
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        serializer = BookingRoomSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                available = RoomAvailability.objects.get(room_id=pk, start=start_date,
+                                                         end=end_time)
+                is_booked = BookingRoom.objects.filter(Q(room_id=pk) & Q(room_availability=available)).count() > 0
+                if is_booked:
+                    return Response({
+                        "error": "Uzr, siz tanlagan vaqtda xona band qilingan"
+                    }, status=status.HTTP_410_GONE)
+                else:
+                    BookingRoom.objects.create(room_id=pk, room_availability=available, resident_name=resident['name'])
+                    return Response({
+                        "message": "Xona muvaffaqiyatli band qilindi"
+                    }, status=status.HTTP_201_CREATED)
+            except ObjectDoesNotExist:
+                data = {
+                    "message": f"Xonaning {start_date.date()} kuni uchun bosh vaqtlari topilmadi"
+                }
+                return Response(data, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
