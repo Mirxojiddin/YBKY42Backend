f2b50b93cee57d9da931387472ef69bb0e30a1ad
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-06-19 19:49:20 +0500

    urls

---
+++ booking_rooms/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from booking_rooms.views import RoomListApiView, RoomAvailabilityApiView, RoomDetailApiView, \
+    BookingRoomApiView
+app_name = "booking_rooms"
+urlpatterns = [
+    path('rooms', RoomListApiView.as_view(), name='rooms'),
+    path('rooms/<int:pk>', RoomDetailApiView.as_view(), name='detail'),
+    path('rooms/<int:pk>/availability', RoomAvailabilityApiView.as_view(), name='availability'),
+    path('rooms/<int:pk>/book', BookingRoomApiView.as_view(), name='book')
+
+]
